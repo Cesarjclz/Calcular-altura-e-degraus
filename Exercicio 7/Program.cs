bfc8de82fc92300c0b041172996b93cc6230d0af
César Aguiar <Cesarphz21@gmail.com>
2021-10-20 12:31:54 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int degrau = 17;
+            int altura;
+            
+            Console.WriteLine("Sabendo que cada degrau tem " + degrau + "cm! Em centimetros digite o valor da escada: ");
+
+
+            altura = int.Parse(Console.ReadLine());
+            altura = altura / degrau;
+            Console.WriteLine("A quantida de degraus que voce precisa é "+altura);
+
+        }
+    }
+}
